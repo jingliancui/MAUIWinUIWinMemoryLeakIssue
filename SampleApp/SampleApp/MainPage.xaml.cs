2af33f4e883ae63544fe83a9e5aa7f400798fcc8
Eric <ericcui@qq.com>
2024-08-11 21:22:24 +0800

    add code

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NewWindowsBtn_Clicked(object sender, EventArgs e)
+        {
+            for (int i = 0; i < int.Parse(PageNumber.Text); i++)
+            {
+                Application.Current?.OpenWindow(new Window(new WebPage()));
+            }
+        }
+
+        private void CloseWindowsBtn_Clicked(object sender, EventArgs e)
+        {
+            var wins = Application.Current!.Windows.Where(d => d.Page is WebPage).ToList();
+            var count = wins.Count();
+            for (int i = 0; i < count; i++)
+            {
+                Application.Current.CloseWindow(wins[i]);
+            }
+        }
+    }
+
+}
---
+++ SampleApp/SampleApp/WebPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SampleApp;
+
+public partial class WebPage : ContentPage
+{
+	public WebPage()
+	{
+		InitializeComponent();
+	}
+}
